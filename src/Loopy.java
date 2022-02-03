c92643a5d3ce519cadce2a4500563e33b2116f08
Alexandra Bystrova <saobyjob90@gmail.com>
2022-02-03 17:43:28 +0300

    First Commit

---
+++ src/Loopy.java
@@ -0,0 +1,12 @@
+public class Loopy {
+        public static void main(String[] args) {
+            int i = 45;
+            while (i < 350) { i++; }
+
+
+
+
+            System.out.println("Value i = " + i);
+        }
+    }
+
